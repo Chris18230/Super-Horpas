52ecb4fb448f4a6e6ed690f664855b5959bc3a16
Miles Winkler <miles3199@gmail.com>
2018-02-20 14:33:56 +0100

    titel geändert

--- src/at/spengergasse/superhorpas/Main.java
+++ src/at/spengergasse/superhorpas/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Super Horpas");
+			primaryStage.setTitle("Super Herpes");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
