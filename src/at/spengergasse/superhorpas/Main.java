cdb9af2b6c1bb8eeec440873521529e307be6421
Chris18230 <36665236+Chris18230@users.noreply.github.com>
2018-03-06 14:54:42 +0100

    bg image

--- src/at/spengergasse/superhorpas/Main.java
+++ src/at/spengergasse/superhorpas/Main.java
@@ -1,10 +1,18 @@
 package at.spengergasse.superhorpas;
 	
+
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.image.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
+
 
 
 public class Main extends Application {
@@ -12,11 +20,15 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1024,760);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Super Herpes");
+			primaryStage.setTitle("Super Horpas");
 			Image image = new Image("/at/spengergasse/superhorpas/icon.png");
+			Image imgBackground = new Image(getClass().getResource("/at/spengergasse/superhorpas/launcher_bg.jpeg").toExternalForm());
+			BackgroundImage backgroundImage = new BackgroundImage(imgBackground, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+	        Background background = new Background(backgroundImage);
+	        root.setBackground(background);
 			primaryStage.getIcons().add(image);
 			primaryStage.show();
 		} catch(Exception e) {
